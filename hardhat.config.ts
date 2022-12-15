0bd1bf8713a06fcf18c1808145c9ecc47a5aeeab
95decode <95decode@gmail.com>
2022-12-16 00:34:19 +0900

    Add optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,23 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    compilers: [{
+      version: "0.8.17",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+          details: {
+            yul: true,
+            yulDetails : {
+              stackAllocation : true
+            },
+          },
+        },
+      },
+    }],
+  },
 };
 
 export default config;
