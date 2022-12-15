35788d0b9c410e0c03419c1eaf57e9e9bf763361
95decode <95decode@gmail.com>
2022-12-16 00:33:41 +0900

    Delete trait number

--- contracts/library/Seed.sol
+++ contracts/library/Seed.sol
@@ -3,11 +3,9 @@ pragma solidity ^0.8.17;
 
 contract Seed {
     bytes private _salt;
-    uint256 internal _traitNum;
 
-    constructor(bytes memory salt, uint256 traitNum) {
+    constructor(bytes memory salt) {
         _salt = salt;
-        _traitNum = traitNum;
     }
 
     function generateSeed(uint256 value) internal view returns (uint256) {
@@ -18,8 +16,6 @@ contract Seed {
                     _salt
                 )
             )
-        ) >> (
-            2 ** 8 - 2 * (_traitNum * 2 ** 2)
-        );
+        ) >> 216;
     }
 }
