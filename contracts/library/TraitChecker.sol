96922b0c5373ebbabd94bbbff4814377391e7fe8
95decode <95decode@gmail.com>
2022-12-28 02:27:03 +0900

    Modify background probability

--- contracts/library/TraitChecker.sol
+++ contracts/library/TraitChecker.sol
@@ -3,37 +3,39 @@ pragma solidity ^0.8.17;
 
 library TraitChecker {
     function seedToBackground(uint40 _seed) internal pure returns (string memory) {
-        if (_seed >= 240) {
+        if (_seed >= 206) {
             return "NORMAL_0";
-        } else if (_seed >= 224) {
+        } else if (_seed >= 190) {
             return "NORMAL_1";
-        } else if (_seed >= 208) {
+        } else if (_seed >= 174) {
             return "NORMAL_2";
-        } else if (_seed >= 192) {
+        } else if (_seed >= 158) {
             return "NORMAL_3";
-        } else if (_seed >= 176) {
+        } else if (_seed >= 142) {
             return "NORMAL_4";
-        } else if (_seed >= 160) {
+        } else if (_seed >= 126) {
             return "NORMAL_5";
-        } else if (_seed >= 144) {
+        } else if (_seed >= 110) {
             return "NORMAL_6";
-        } else if (_seed >= 128) {
+        } else if (_seed >= 94) {
             return "NORMAL_7";
-        } else if (_seed >= 112) {
+        } else if (_seed >= 78) {
             return "NORMAL_8";
-        } else if (_seed >= 96) {
+        } else if (_seed >= 62) {
             return "NORMAL_9";
-        } else if (_seed >= 80) {
+        } else if (_seed >= 46) {
             return "NORMAL_A";
-        } else if (_seed >= 64) {
+        } else if (_seed >= 30) {
             return "NORMAL_B";
-        } else if (_seed >= 48) {
+        } else if (_seed >= 14) {
             return "NORMAL_C";
-        } else if (_seed >= 32) {
+        } else if (_seed >= 10) {
             return "NORMAL_D";
-        } else if (_seed >= 16) {
+        } else if (_seed >= 8) {
             return "NORMAL_E";
-        } else if (_seed >= 9) {
+        } else if (_seed >= 6) {
+            return "NORMAL_F";
+        } else if (_seed >= 4) {
             return "SPECIAL_0";
         } else if (_seed >= 2) {
             return "SPECIAL_1";
