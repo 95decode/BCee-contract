d4b36590b4914035cdc401864eeda382235ee121
95decode <95decode@gmail.com>
2023-01-05 00:00:57 +0900

    Add hand traits

--- contracts/library/image/Hands.sol
+++ contracts/library/image/Hands.sol
@@ -1,130 +1,60 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "./Prefix.sol";
-
 library Hands {
-    string constant internal NORMAL_0 = "000000\" d=\"M11 17h1M14 17h1M11 18h1M14 18h1M11 19h1M14 19h1M12 20h2\" />";
-    string constant internal NORMAL_1 = "000000\" d=\"M11 17h1M14 17h1M11 18h1M14 18h1M11 19h1M14 19h1M12 20h2\" /><path stroke=\"#880015\" d=\"M8 18h3M15 18h10M8 19h3M15 19h10\" />";
-    string constant internal NORMAL_2 = "000000\" d=\"M11 17h1M14 17h1M16 17h1M9 18h3M14 18h3M9 19h3M14 19h3M12 20h2M16 20h1\" /><path stroke=\"#7f7f7f\" d=\"M17 18h8M17 19h6\" />";
-    string constant internal NORMAL_3 = "000000\" d=\"M19 13h1M13 14h7M13 15h1M19 15h1M12 16h3M16 16h4M15 17h2M15 18h1M12 19h3\" /><path stroke=\"#7f7f7f\" d=\"M14 15h5M15 16h1\" />";
-    string constant internal NORMAL_4 = "ed1c24\" d=\"M19 10h1M18 11h3M17 12h3M16 13h3M15 14h3M16 15h1M10 19h1M9 20h3M8 21h3M7 22h3M6 23h3M7 24h1\" /><path stroke=\"#7f7f7f\" d=\"M14 14h1M14 15h2M15 16h2M16 17h1M10 18h2M11 19h2M12 20h2\" /><path stroke=\"#000000\" d=\"M13 15h1M14 16h1M11 17h1M15 17h1M12 18h1M15 18h1M13 19h2\" />";
-    string constant internal NORMAL_5 = "000000\" d=\"M13 12h3M13 13h1M16 13h1M13 14h3M13 15h1M12 16h3M15 17h1M15 18h1M12 19h3\" /><path stroke=\"#b5e61d\" d=\"M14 13h2\" />";
-    string constant internal NORMAL_6 = "000000\" d=\"M11 17h1M14 17h1M11 18h1M14 18h1M11 19h1M14 19h1M12 20h2\" /><path stroke=\"#3f48cc\" d=\"M15 19h1M14 20h3M13 21h5M14 22h3M15 23h1\" />";
-    string constant internal NORMAL_7 = "f8acaf\" d=\"M21 16h1\" /><path stroke=\"#f9b5b8\" d=\"M22 16h1M24 16h1\" /><path stroke=\"#f9aeb1\" d=\"M23 16h1M22 17h1\" /><path stroke=\"#000000\" d=\"M4 17h8M14 17h7M4 18h1M11 18h1M14 18h1M4 19h8M14 19h6M12 20h2\" /><path stroke=\"#f8a7aa\" d=\"M21 17h1\" /><path stroke=\"#f9b2b5\" d=\"M23 17h1\" /><path stroke=\"#f9NORMAL_b3\" d=\"M24 17h1\" /><path stroke=\"#fbc9cb\" d=\"M25 17h1\" /><path stroke=\"#b97a57\" d=\"M5 18h6M15 18h5\" /><path stroke=\"#f9b2b4\" d=\"M20 18h1\" /><path stroke=\"#fabcbf\" d=\"M21 18h1\" /><path stroke=\"#fbd2d4\" d=\"M22 18h1\" /><path stroke=\"#f58b8f\" d=\"M23 18h1\" /><path stroke=\"#f5858a\" d=\"M24 18h1\" /><path stroke=\"#f79b9f\" d=\"M25 18h1M21 19h1\" /><path stroke=\"#4a090b\" d=\"M20 19h1\" /><path stroke=\"#f8abae\" d=\"M22 19h1\" /><path stroke=\"#f8a2a6\" d=\"M23 19h1\" /><path stroke=\"#fcd6d7\" d=\"M24 19h1\" /><path stroke=\"#f8a8ab\" d=\"M25 19h1\" /><path stroke=\"#f9afb2\" d=\"M21 20h1\" /><path stroke=\"#f9b6b9\" d=\"M22 20h1\" /><path stroke=\"#f9b9bb\" d=\"M23 20h1\" /><path stroke=\"#fcd3d5\" d=\"M24 20h1\" />";
-
-    string constant internal NORMAL_8 = "";
-    string constant internal NORMAL_9 = "";
-    string constant internal NORMAL_A = "";
-    string constant internal NORMAL_B = "";
-    string constant internal NORMAL_C = "";
-    string constant internal NORMAL_D = "";
-    string constant internal NORMAL_E = "";
-
-    string constant internal SPECIAL_0 = "";
-    string constant internal SPECIAL_1 = "";
-    string constant internal SPECIAL_2 = "";
-    string constant internal SPECIAL_3 = "";
+    string constant internal NONE = "<path stroke=\"#000000\" d=\"M11 17h1M14 17h1M11 18h1M14 18h1M11 19h1M14 19h1M12 20h2\" />";
+    string constant internal BAR = "<path stroke=\"#47080b\" d=\"M8 18h3M15 18h10M8 19h3M15 19h10\" />";
+    string constant internal YELLOW_CARD = "<path stroke=\"#fff200\" d=\"M15 19h1M14 20h3M13 21h5M14 22h3M15 23h1\" />";
+    string constant internal RED_CARD = "<path stroke=\"#ed1c24\" d=\"M15 19h1M14 20h3M13 21h5M14 22h3M15 23h1\" />";
+    string constant internal FLAG = "<path stroke=\"#000000\" d=\"M13 12h3M13 13h1M16 13h1M13 14h3M13 15h1M12 16h3M15 17h1M15 18h1M12 19h3\" /><path stroke=\"#b5e61d\" d=\"M14 13h2\" />";
+    string constant internal GOOD = "<path stroke=\"#000000\" d=\"M14 15h1M12 16h2M15 16h1M15 17h1M15 18h1M12 19h3\" />";
+    string constant internal SICKLE = "<path stroke=\"#000000\" d=\"M11 17h1M14 17h1M4 18h8M14 18h15M4 19h8M14 19h13M28 19h1M12 20h2M28 20h1M28 21h1M27 22h2M27 23h1M26 24h2M25 25h2M23 26h3\" /><path stroke=\"#7f7f7f\" d=\"M27 19h1M27 20h1M26 21h2M26 22h1M25 23h2M24 24h2M23 25h2M22 26h1\" />";
+    string constant internal MATCH = "<path stroke=\"#ed1c24\" d=\"M14 14h2\" /><path stroke=\"#b97a57\" d=\"M14 15h2M15 16h1M15 19h1M14 20h2\" /><path stroke=\"#000000\" d=\"M12 16h3M15 17h1M15 18h1M12 19h3\" />";
+    string constant internal BRIEFCASE= "<path stroke=\"#000000\" d=\"M11 17h1M14 17h1M11 18h1M14 18h1M11 19h1M14 19h1M10 20h6M10 21h2M14 21h2M8 22h10M8 23h1M11 23h1M14 23h1M17 23h1M8 24h1M11 24h4M17 24h1M8 25h1M17 25h1M8 26h10\" /><path stroke=\"#7f7f7f\" d=\"M9 23h2M15 23h2M9 24h2M15 24h2M9 25h8\" /><path stroke=\"#c3c3c3\" d=\"M12 23h2\" />";
+    string constant internal DAGGER = "<path stroke=\"#000000\" d=\"M11 17h1M14 17h1M16 17h1M9 18h3M14 18h3M9 19h3M14 19h3M12 20h2M16 20h1\" /><path stroke=\"#7f7f7f\" d=\"M17 18h8M17 19h6\" />";
+    string constant internal HAMMER = "<path stroke=\"#000000\" d=\"M21 15h3M20 16h1M24 16h1M4 17h8M14 17h7M24 17h1M4 18h1M11 18h1M14 18h1M24 18h1M4 19h8M14 19h7M24 19h1M12 20h2M20 20h1M24 20h1M21 21h3\" /><path stroke=\"#ed1c24\" d=\"M21 16h3M21 17h3M20 18h4M21 19h3M21 20h3\" /><path stroke=\"#b97a57\" d=\"M5 18h6M15 18h5\" />";
+    string constant internal HEART = "<path stroke=\"#ed1c24\" d=\"M14 14h2M17 14h2M14 15h5M15 16h3M16 17h1\" /><path stroke=\"#000000\" d=\"M12 16h3M15 17h1M15 18h1M12 19h3\" />";
+    string constant internal GUN = "<path stroke=\"#000000\" d=\"M19 13h1M13 14h7M13 15h1M19 15h1M12 16h3M16 16h4M15 17h2M15 18h1M12 19h3\" /><path stroke=\"#7f7f7f\" d=\"M14 15h5M15 16h1\" />";
 
     function constructHands(uint256 seed) internal pure returns (string memory) {
         uint40 _seed = uint40(seed << 8*4);
         _seed = _seed >> 8*4;
 
-        if (_seed >= 240) {
+        if (_seed >= 206) {
+            return NONE;
+        } else if (_seed >= 181) {
             return string.concat(
-                Prefix.prefix(), 
-                NORMAL_0
+                NONE,
+                BAR
             );
-        } else if (_seed >= 224) {
+        } else if (_seed >= 156) {
             return string.concat(
-                Prefix.prefix(), 
-                NORMAL_1
+                NONE,
+                YELLOW_CARD
             );
-        } else if (_seed >= 208) {
+        } else if (_seed >= 131) {
             return string.concat(
-                Prefix.prefix(), 
-                NORMAL_2
+                NONE,
+                RED_CARD
             );
-        } else if (_seed >= 192) {
-            return string.concat(
-                Prefix.prefix(), 
-                NORMAL_3
-            );
-        } else if (_seed >= 176) {
-            return string.concat(
-                Prefix.prefix(), 
-                NORMAL_4
-            );
-        } else if (_seed >= 160) {
-            return string.concat(
-                Prefix.prefix(), 
-                NORMAL_5
-            );
-        } else if (_seed >= 144) {
-            return string.concat(
-                Prefix.prefix(), 
-                NORMAL_6
-            );
-        } else if (_seed >= 128) {
-            return string.concat(
-                Prefix.prefix(), 
-                NORMAL_7
-            );
-        } else if (_seed >= 112) {
-            return string.concat(
-                Prefix.prefix(), 
-                NORMAL_0
-            );
-        } else if (_seed >= 96) {
-            return string.concat(
-                Prefix.prefix(), 
-                NORMAL_0
-            );
-        } else if (_seed >= 80) {
-            return string.concat(
-                Prefix.prefix(), 
-                NORMAL_0
-            );
-        } else if (_seed >= 64) {
-            return string.concat(
-                Prefix.prefix(), 
-                NORMAL_0
-            );
-        } else if (_seed >= 48) {
-            return string.concat(
-                Prefix.prefix(), 
-                NORMAL_0
-            );
-        } else if (_seed >= 32) {
-            return string.concat(
-                Prefix.prefix(), 
-                NORMAL_0
-            );
-        } else if (_seed >= 16) {
-            return string.concat(
-                Prefix.prefix(), 
-                NORMAL_0
-            );
-        } else if (_seed >= 9) {
-            return string.concat(
-                Prefix.prefix(), 
-                NORMAL_0
-            );
-        } else if (_seed >= 2) {
-            return string.concat(
-                Prefix.prefix(), 
-                NORMAL_0
-            );
-        } else if (_seed >= 1) {
-            return string.concat(
-                Prefix.prefix(), 
-                NORMAL_0
-            );
+        } else if (_seed >= 106) {
+            return FLAG;
+        } else if (_seed >= 81) {
+            return GOOD;
+        } else if (_seed >= 61) {
+            return SICKLE;
+        } else if (_seed >= 41) {
+            return MATCH;
+        } else if (_seed >= 31) {
+            return BRIEFCASE;
+        } else if (_seed >= 21) {
+            return DAGGER;
+        } else if (_seed >= 11) {
+            return HAMMER;
+        } else if (_seed >= 5) {
+            return HEART;
         } else {
-            return string.concat(
-                Prefix.prefix(), 
-                NORMAL_0
-            );
+            return GUN;
         }
     }
 }
