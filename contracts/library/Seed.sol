f37ede32e4dc0a9499d49436397969225e184903
95decode <95decode@gmail.com>
2023-01-05 23:36:34 +0900

    Add comments

--- contracts/library/Seed.sol
+++ contracts/library/Seed.sol
@@ -4,6 +4,14 @@ pragma solidity ^0.8.17;
 contract Seed {
     bytes32 private _salt;
 
+    // BCee creates a random seed using salt and tokenId, but this is 
+    // not truly random and can be predicted by anyone.
+    // And it doesn't really matter. This is because if all NFTs limited 
+    // to 1000 are minted, they will not be used after that.
+    // In addition, values such as block.timestamp and block.number 
+    // can be used as seeds at the time each NFT is minted, 
+    // but even this is sufficiently predictable, and a new seed is 
+    // stored at that time, so the user's gas fee increases.
     constructor(bytes32 salt) {
         _salt = salt;
     }
